ebd79e6ab07b8dd5d7bfc0e5084c584c191423a1
VuHoaBinh <hoabinh.vippro63@gmail.com>
2024-04-13 11:26:07 +0700

    update: htmlToJS

--- index.js
+++ index.js
@@ -103,3 +103,76 @@ for (let index = 0; index < products.length; index++) {
 }
 
 document.getElementById("product-list").innerHTML = htmlString;
+
+const cardData = [
+  {
+    title: "IPhone 2025",
+    originalPrice: "2.600.000",
+    discountedPrice: "2.200.000",
+    imgUrl:
+      "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-13_2_.png",
+  },
+];
+
+function createCard(cardInfo) {
+  const card = document.createElement("div");
+  card.classList.add("card", "mb-3");
+
+  const row = document.createElement("div");
+  row.classList.add("row", "g-0");
+
+  const colImg = document.createElement("div");
+  colImg.classList.add("col-md-4");
+
+  const img = document.createElement("img");
+  img.src = cardInfo.imgUrl;
+  img.classList.add("img-fluid", "rounded-start");
+  img.alt = "...";
+
+  const colBody = document.createElement("div");
+  colBody.classList.add("col-md-8");
+
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+
+  const title = document.createElement("h5");
+  title.classList.add("card-title");
+  title.textContent = cardInfo.title;
+
+  const priceWrapper = document.createElement("div");
+
+  const originalPrice = document.createElement("p");
+  originalPrice.classList.add(
+    "my-1",
+    "text-decoration-line-through",
+    "fst-italic"
+  );
+  originalPrice.textContent = cardInfo.originalPrice;
+
+  const discountedPrice = document.createElement("p");
+  discountedPrice.classList.add("my-1", "text-danger", "fw-bold");
+  discountedPrice.textContent = cardInfo.discountedPrice;
+
+  card.appendChild(row);
+  row.appendChild(colImg);
+  colImg.appendChild(img);
+  row.appendChild(colBody);
+  colBody.appendChild(cardBody);
+  cardBody.appendChild(title);
+  cardBody.appendChild(priceWrapper);
+  priceWrapper.appendChild(originalPrice);
+  priceWrapper.appendChild(discountedPrice);
+
+  return card;
+}
+
+function addCardsToDOM() {
+  const cardContainer = document.getElementById("card-container");
+
+  cardData.forEach((cardInfo) => {
+    const card = createCard(cardInfo);
+    cardContainer.appendChild(card);
+  });
+}
+
+addCardsToDOM();
